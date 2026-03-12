9950fa2702ed69ba8057be373c4b848655715c76
Nandy Bâ <nandyba@yahoo.fr>
2026-03-12 16:57:52 +0100

    feat: add borrow wstETH test after payload execution part 1

--- src/20260312_Multi_ReEnableWstETHBorrowCapsAfterCapoIncident/AaveV3Ethereum_ReEnableWstETHBorrowCapsAfterCapoIncident_20260312.t.sol
+++ src/20260312_Multi_ReEnableWstETHBorrowCapsAfterCapoIncident/AaveV3Ethereum_ReEnableWstETHBorrowCapsAfterCapoIncident_20260312.t.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {AaveV3Ethereum, AaveV3EthereumAssets} from 'aave-address-book/AaveV3Ethereum.sol';
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {IPool} from 'aave-v3-origin/contracts/interfaces/IPool.sol';
 
 import 'forge-std/Test.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
@@ -29,6 +31,27 @@ contract AaveV3Ethereum_ReEnableWstETHBorrowCapsAfterCapoIncident_20260312_Test 
       'AaveV3Ethereum_ReEnableWstETHBorrowCapsAfterCapoIncident_20260312',
       AaveV3Ethereum.POOL,
       address(proposal)
+    );
+  }
+
+  function test_borrowWstETHAfterProposal() public {
+    executePayload(vm, address(proposal), AaveV3Ethereum.POOL);
+    skip(3600);
+
+    address user = 0xa5CF85717C958d2f7875D7B68EF845bD897215Fc;
+    IPool pool = IPool(address(AaveV3Ethereum.POOL));
+
+    uint256 borrowAmount = 1e18;
+    uint256 debtBefore = IERC20(AaveV3EthereumAssets.wstETH_V_TOKEN).balanceOf(user);
+
+    vm.prank(user);
+    pool.borrow(AaveV3EthereumAssets.wstETH_UNDERLYING, borrowAmount, 2, 0, user);
+
+    uint256 debtAfter = IERC20(AaveV3EthereumAssets.wstETH_V_TOKEN).balanceOf(user);
+    assertGe(
+      debtAfter - debtBefore,
+      borrowAmount,
+      'User debt should increase by at least 1 wstETH'
     );
   }
 }
