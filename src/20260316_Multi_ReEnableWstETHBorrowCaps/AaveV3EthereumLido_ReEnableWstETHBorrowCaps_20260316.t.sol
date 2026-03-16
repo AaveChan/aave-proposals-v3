5f66db139d1ea3f0d8ad1e1b0de0767adc38a322
Nandy Bâ <nandyba@yahoo.fr>
2026-03-16 12:02:08 +0100

    improve wstETH Prime borrow cap tests

--- src/20260316_Multi_ReEnableWstETHBorrowCaps/AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316.t.sol
+++ src/20260316_Multi_ReEnableWstETHBorrowCaps/AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316.t.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {AaveV3EthereumLido} from 'aave-address-book/AaveV3EthereumLido.sol';
+import {AaveV3EthereumLido, AaveV3EthereumLidoAssets} from 'aave-address-book/AaveV3EthereumLido.sol';
 
 import 'forge-std/Test.sol';
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {IPool} from 'aave-v3-origin/contracts/interfaces/IPool.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/src/ProtocolV3TestBase.sol';
 import {AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316} from './AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316.sol';
 
@@ -27,6 +29,26 @@ contract AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316_Test is ProtocolV3
       'AaveV3EthereumLido_ReEnableWstETHBorrowCaps_20260316',
       AaveV3EthereumLido.POOL,
       address(proposal)
+    );
+  }
+
+  function test_borrowWstETHAfterProposal() public {
+    executePayload(vm, address(proposal));
+
+    address user = 0x2e2A6815ec3E7280E01ef03ED26B3Bb13F3920fb; // user with an hf of 1.15 on Lido with  tETH/wstETH e-mode activated
+    IPool pool = IPool(address(AaveV3EthereumLido.POOL));
+
+    uint256 borrowAmount = 1e18;
+    uint256 debtBefore = IERC20(AaveV3EthereumLidoAssets.wstETH_V_TOKEN).balanceOf(user);
+
+    vm.prank(user);
+    pool.borrow(AaveV3EthereumLidoAssets.wstETH_UNDERLYING, borrowAmount, 2, 0, user);
+
+    uint256 debtAfter = IERC20(AaveV3EthereumLidoAssets.wstETH_V_TOKEN).balanceOf(user);
+    assertGe(
+      debtAfter - debtBefore,
+      borrowAmount,
+      'User debt should increase by at least 1 wstETH'
     );
   }
 }
