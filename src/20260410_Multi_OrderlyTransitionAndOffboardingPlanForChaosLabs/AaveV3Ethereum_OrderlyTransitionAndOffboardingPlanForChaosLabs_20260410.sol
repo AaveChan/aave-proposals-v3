2a9a55ecbbf17a26ba65e9fcc90ab415596019be
Rozengarden <github@rozengarden.eu>
2026-04-10 19:14:27 +0200

    cancel chaos labs stream and send 30 days in bulk

--- src/20260410_Multi_OrderlyTransitionAndOffboardingPlanForChaosLabs/AaveV3Ethereum_OrderlyTransitionAndOffboardingPlanForChaosLabs_20260410.sol
+++ src/20260410_Multi_OrderlyTransitionAndOffboardingPlanForChaosLabs/AaveV3Ethereum_OrderlyTransitionAndOffboardingPlanForChaosLabs_20260410.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import {IProposalGenericExecutor} from 'aave-helpers/src/interfaces/IProposalGenericExecutor.sol';
 import {MiscEthereum} from 'aave-address-book/MiscEthereum.sol';
 import {IAgentHub} from '../interfaces/chaos-agents/IAgentHub.sol';
-
+import {AaveV3EthereumLido, AaveV3EthereumLidoAssets} from 'aave-address-book/AaveV3EthereumLido.sol';
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
 /**
  * @title Orderly Transition and Offboarding Plan for Chaos Labs
  * @author ChaosLabs (implemented by Aavechan Initiative @aci via Skyward)
@@ -14,11 +15,26 @@ import {IAgentHub} from '../interfaces/chaos-agents/IAgentHub.sol';
 contract AaveV3Ethereum_OrderlyTransitionAndOffboardingPlanForChaosLabs_20260410 is
   IProposalGenericExecutor
 {
+  address public constant CHAOS_LABS = 0xbC540e0729B732fb14afA240aA5A047aE9ba7dF0; // stream 100073 recipient
+  uint256 public constant PREVIOUS_STREAM = 100073;
+  uint256 public constant AMOUNT_PER_SECOND = 80859969558599695; // from stream 100073
+
   function execute() external {
     // custom code goes here
     uint256 agentCount = IAgentHub(MiscEthereum.AGENT_HUB).getAgentCount();
     for (uint256 i = 0; i < agentCount; i++) {
       IAgentHub(MiscEthereum.AGENT_HUB).setAgentEnabled(i, false);
     }
+
+    // chaos labs mentionned cutting the stream themselves
+    try AaveV3EthereumLido.COLLECTOR.cancelStream(PREVIOUS_STREAM) {}
+    catch {}
+
+    // bulk transfer
+    AaveV3EthereumLido.COLLECTOR.transfer(
+      IERC20(AaveV3EthereumLidoAssets.GHO_A_TOKEN),
+      CHAOS_LABS,
+      AMOUNT_PER_SECOND * 30 days
+    );
   }
 }
